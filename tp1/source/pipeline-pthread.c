f61dee885b592d20a8222f4920f538c0aabc3911
Thomas Caron <thomas.caron@polymtl.ca>
2022-09-13 16:34:33 -0400

    Finished TBB of TP1

--- tp1/source/pipeline-pthread.c
+++ tp1/source/pipeline-pthread.c
@@ -1,8 +1,103 @@
 #include <stdio.h>
+#include "pthread.h"
 
 #include "filter.h"
 #include "pipeline.h"
+#include "queue.h"
+
+#define NUM_STEPS 4
+#define NUM_THREADS 16
+#define QUEUE_SIZE 400
+
+const queue_t *scale_up_queue, *sharpen_queue, *sobel_queue, *save_queue;
+pthread_mutex_t* mutex;
+pthread_cond_t* pcond;
+
+void* read_all_images(void* args) {
+	image_dir_t* image_dir = (image_dir_t*) args;
+
+	while (1) {
+		pthread_mutex_lock(mutex);
+		image_t* image = image_dir_load_next(image_dir);
+		pthread_mutex_unlock(mutex);
+
+		if (image == NULL) {
+			pthread_cond_signal(pcond);
+			break;
+		}
+		queue_push(scale_up_queue, image);
+	}
+}
+
+
+void* save_all_images(void* args) {
+	image_dir_t* image_dir = (image_dir_t*) args;
+	while (1) {
+		image_t* image = (image_t*) queue_pop(save_queue);
+		if (image == NULL) {
+			break;
+		}
+		pthread_mutex_lock(mutex);
+		image_dir_save(image_dir, image);
+		pthread_mutex_unlock(mutex);
+		printf(".");
+		fflush(stdout);
+		image_destroy(image);
+	}
+}
+
+void* scale_up(void* args) {
+	image_t* image = (image_t*) queue_pop(scale_up_queue);
+	if (image == NULL) {
+		return NULL;
+	}
+	image_t* modified = filter_scale_up(image, 2);
+	image_destroy(image);
+	queue_push(sharpen_queue, modified);
+}
+
+void* sharpen(void* args) {
+	image_t* image = (image_t*) queue_pop(sharpen_queue);
+	if (image == NULL) {
+		return NULL;
+	}
+	image_t* modified = filter_sharpen(image);
+	image_destroy(image);
+	queue_push(sobel_queue, modified);
+}
+
+void* sobel(void* args) {
+	image_t* image = (image_t*) queue_pop(sobel_queue);
+	if (image == NULL) {
+		return NULL;
+	}
+	image_t* modified = filter_sobel(image);
+	image_destroy(image);
+	queue_push(save_queue, modified);
+}
+
 
 int pipeline_pthread(image_dir_t* image_dir) {
-	return -1;
+	pthread_mutex_init(mutex, NULL);
+	pthread_cond_init(pcond, NULL);
+
+	scale_up_queue = queue_create(QUEUE_SIZE);
+	sharpen_queue = queue_create(QUEUE_SIZE);
+	sobel_queue = queue_create(QUEUE_SIZE);
+	save_queue = queue_create(QUEUE_SIZE);
+
+	pthread_t threads[4][NUM_THREADS];
+
+	pthread_t read_thread;
+	pthread_t save_thread;
+
+	pthread_create(&read_thread, NULL, read_all_images, image_dir);
+
+	while (1) {
+		// TODO:
+	}
+
+	pthread_mutex_destroy(mutex);
+	
+	return 0;
 }
--- tp1/source/pipeline-tbb.cpp
+++ tp1/source/pipeline-tbb.cpp
@@ -1,10 +1,80 @@
 #include <stdio.h>
+#include "tbb/pipeline.h"
 
 extern "C" {
 #include "filter.h"
 #include "pipeline.h"
 }
 
+class TBBLoadNext {
+    image_dir_t* image_dir;
+public:
+    TBBLoadNext(image_dir_t* image_dir) : image_dir(image_dir) {}
+
+    image_t* operator()(tbb::flow_control& fc) const {
+        image_t* out = image_dir_load_next(image_dir);
+        if (out == NULL) {
+            fc.stop();
+        }
+        return out;
+    }
+};
+
+class TBBScaleUp {
+public:
+    TBBScaleUp() {}
+    image_t* operator()(image_t* in) const {
+        image_t* out = filter_scale_up(in, 2);
+        if (out == NULL) {
+            exit(-1);
+        }
+        image_destroy(in);
+        return out;
+    }
+};
+
+class TBBSharpen {
+public:
+    TBBSharpen() {}
+    image_t* operator()(image_t* in) const {
+        image_t* out = filter_sharpen(in);
+        if (out == NULL) {
+            exit(-1);
+        }
+        image_destroy(in);
+        return out;
+    }
+};
+
+class TBBSobel {
+public:
+    TBBSobel() {}
+    image_t* operator()(image_t* in) const {
+        image_t* out = filter_sobel(in);
+        image_destroy(in);
+        return out;
+    }
+};
+
+class TBBSave {
+    image_dir_t* image_dir;
+public:
+    TBBSave(image_dir_t* image_dir) : image_dir(image_dir) {}
+
+    void operator()(image_t* in) const {
+        image_dir_save(image_dir, in);
+        image_destroy(in);
+    }
+};
+
 int pipeline_tbb(image_dir_t* image_dir) {
-    return -1;
+    tbb::parallel_pipeline(
+        16,
+        tbb::make_filter<void, image_t*>(tbb::filter::serial, TBBLoadNext(image_dir)) &
+        tbb::make_filter<image_t*, image_t*>(tbb::filter::parallel, TBBScaleUp()) &
+        tbb::make_filter<image_t*, image_t*>(tbb::filter::parallel, TBBSharpen()) &
+        tbb::make_filter<image_t*, image_t*>(tbb::filter::parallel, TBBSobel()) &
+        tbb::make_filter<image_t*, void>(tbb::filter::serial, TBBSave(image_dir))
+    );
+    return 0;
 }
