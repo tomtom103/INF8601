e5fe942c85ae0a72b012284db52e636a66d77523
Thomas Caron <thcarh@l4712-16.info.polymtl.ca>
2022-10-25 12:30:48 -0400

    Temp

--- tp2/source/sinoscope-opencl.c
+++ tp2/source/sinoscope-opencl.c
@@ -5,12 +5,18 @@
 
 int sinoscope_opencl_init(sinoscope_opencl_t* opencl, cl_device_id opencl_device_id, unsigned int width,
 			  unsigned int height) {
-	return -1;
+	cl_int error = 0;
+	opencl->device_id = opencl_device_id;
+	opencl->context = clCreateContext(0, 1, &opencl_device_id, NULL, NULL, &error);
+	if (error != CL_SUCCESS) return -1;
+	opencl->queue = clCreateCommandQueue(opencl->context, opencl_device_id, 0, &error);
+	if (error != CL_SUCCESS) return -1;
+	return 0;
 }
 
 void sinoscope_opencl_cleanup(sinoscope_opencl_t* opencl)
 {
-
+	
 }
 
 int sinoscope_image_opencl(sinoscope_t* sinoscope) {
